06a33ca94ee284be1c61b60ddd935a7ce7826488
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-09-02 02:35:37 +0900

    2 완성

--- app/src/main/java/com/example/rejeekbangcopy_recyclerview_20220902/MainActivity.kt
+++ app/src/main/java/com/example/rejeekbangcopy_recyclerview_20220902/MainActivity.kt
@@ -3,14 +3,37 @@ package com.example.rejeekbangcopy_recyclerview_20220902
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.rejeekbangcopy_recyclerview_20220902.adapters.RoomRecyclerViewAdapter
 import com.example.rejeekbangcopy_recyclerview_20220902.databinding.ActivityMainBinding
+import com.example.rejeekbangcopy_recyclerview_20220902.datas.RoomData
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
+    val mRoomList = ArrayList<RoomData>()
+
+    lateinit var mRoomAdapter : RoomRecyclerViewAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        mRoomList.add( RoomData(8000, "마포구 서교동", 1, "망원/홍대역 풀옵션 넓은 원룸") )
+        mRoomList.add( RoomData(28000, "마포구 서교동", 5, "홍대입구역 풀옵션 투룸") )
+        mRoomList.add( RoomData(12000, "마포구 서교동", 2, "망원/홍대역 인근 신축 원룸 전세") )
+        mRoomList.add( RoomData(12000, "마포구 망원동", 3, "홍대역 풀옵션 넓은 원룸") )
+        mRoomList.add( RoomData(15000, "마포구 망원동", 5, "테라스가 넓은 풀옵션 넓은 원룸") )
+        mRoomList.add( RoomData(13000, "마포구 망원동", 3, "넓고 반듯한 신축 원룸") )
+        mRoomList.add( RoomData(9000, "마포구 연남동", 2, "홍대역 풀옵션 넓은 원룸") )
+        mRoomList.add( RoomData(7500, "마포구 연남동", -2, "연남동 연습 가능 연습실") )
+        mRoomList.add( RoomData(26000, "마포구 연남동", 3, "강추!! 홍대역 테라스 넓은 원룸") )
+        mRoomList.add( RoomData(5500, "마포구 연남동", 0, "홍대역 풀옵션 원룸") )
+
+        mRoomAdapter = RoomRecyclerViewAdapter(this, mRoomList)
+        binding.roomRecyclerView.adapter = mRoomAdapter
+        binding.roomRecyclerView.layoutManager = LinearLayoutManager(this)
+
     }
 }
--- app/src/main/java/com/example/rejeekbangcopy_recyclerview_20220902/adapters/RoomRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/rejeekbangcopy_recyclerview_20220902/adapters/RoomRecyclerViewAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.rejeekbangcopy_recyclerview_20220902.R
 import com.example.rejeekbangcopy_recyclerview_20220902.datas.RoomData
@@ -13,11 +14,21 @@ class RoomRecyclerViewAdapter(
     val mList: List<RoomData>
 ) : RecyclerView.Adapter<RoomRecyclerViewAdapter.MyViewHolder>() {
 
+//   한 칸의 xml은 itemView변수 안에 담겨있고  한 칸의 데이터(RoomData) item이라는 변수 안에 담겨있다
     inner class MyViewHolder(view : View) : RecyclerView.ViewHolder(view){
         fun bind(item : RoomData){
 
-        }
+            val priceTxt = itemView.findViewById<TextView>(R.id.priceTxt)
+            val addressTxt = itemView.findViewById<TextView>(R.id.addressTxt)
+            val levelTxt = itemView.findViewById<TextView>(R.id.levelTxt)
+            val descriptionTxt = itemView.findViewById<TextView>(R.id.descriptionTxt)
 
+            priceTxt.text = item.getFormattedPrice()
+            addressTxt.text = "${item.address}, "
+            levelTxt.text = item.getFormattedLevel()
+            descriptionTxt.text = item.description
+
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
